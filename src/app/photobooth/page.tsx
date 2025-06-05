9bad660200915bff7a8f0ec54c523d889612373d
favils <fatimavillena@gmail.com>
2025-06-05 17:17:25 -0400

    live camera mounted

--- src/app/photobooth/page.tsx
+++ src/app/photobooth/page.tsx
@@ -1,15 +1,31 @@
+"use client"
 import localFont from "next/font/local";
+import { useEffect, useRef } from "react";
 
 export default function Photobooth() {
+  
+  useEffect(() => {
+    const video = document.createElement("video");
+    video.id = "cam";
+    video.autoplay = true;
+    video.muted = true;
+
+    document.getElementById("livecam")?.appendChild(video);
+
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+      video.srcObject = stream;
+    })
+  }, []);
+
   return (
     <div className="h-[100vh] flex items-center justify-center">
       <div
         className="w-[95%] h-[75%] rounded-full flex flex-col items-center justify-center bg-cover bg-center"
         style={{ backgroundImage: 'url("/bg.png")' }}
       >
-        
-        <video></video>
-        
+
+        <div id="livecam"></div>
+
       </div>
     </div>
   );
